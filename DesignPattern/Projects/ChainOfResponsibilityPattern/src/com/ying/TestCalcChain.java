aa17a82be611f75c0df5c665a29e638f6e4ecf5e
hsnyxfhyqhyh <xfyang46@gmail.com>
2019-06-25 14:32:17 -0400

    check in the final change for chain of responsibility pattern example.

--- DesignPattern/Projects/ChainOfResponsibilityPattern/src/com/ying/TestCalcChain.java
+++ DesignPattern/Projects/ChainOfResponsibilityPattern/src/com/ying/TestCalcChain.java
@@ -20,8 +20,9 @@ public class TestCalcChain {
 		// Define the data in the Numbers Object
 		// and send it to the first Object in the chain
 
-		Numbers request = new Numbers(4, 2, "add");
-
+		//Numbers request = new Numbers(4, 2, "sub");
+		//Numbers request = new Numbers(4, 2, "add");
+		Numbers request = new Numbers(4, 2, "div");
 		chainCalc1.calculate(request);
 
 	}
