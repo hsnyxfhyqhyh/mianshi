b945c8aa234bae9d9da36e5952d282ecfa661a07
hsnyxfhyqhyh <xfyang46@gmail.com>
2019-07-14 09:50:05 -0400

    checkin exercise for class , construtor , overloading, etc

---
+++ javaCores/_Exercise/com/ying/exercise/core/_01ClassConstructor.java
@@ -0,0 +1,25 @@
+package com.ying.exercise.core;
+
+public class _01ClassConstructor {
+
+	public static void main(String[] args) {
+	}
+
+}
+
+//TODO 
+/*
+ * create a class "Calc" (addition operation only) 
+ * 	with 2 properties, num1 & num2; 
+ * 	with 1 method add to return a result of addition to num1 &numb2, with 0 parameters;
+ *  
+ *  with overloaded constructors
+ *  	one default, 
+ *  	one with 2 int parameters.
+ *  
+ *  with get/set method for property num1, num2
+ *  	
+ *
+ */
+
+
---
+++ javaCores/_Exercise/docs/Readme.txt
@@ -0,0 +1,15 @@
+_01ClassConstructor
+	//TODO 
+		/*
+		 * create a class "Calc" (addition operation only) 
+		 * 	with 2 properties, num1 & num2; 
+		 * 	with 1 method add to return a result of addition to num1 &numb2, with 0 parameters;
+		 *  
+		 *  with overloaded constructors
+		 *  	one default, 
+		 *  	one with 2 int parameters.
+		 *  
+		 *  with get/set method for property num1, num2
+		 *  	
+		 *
+		 */
