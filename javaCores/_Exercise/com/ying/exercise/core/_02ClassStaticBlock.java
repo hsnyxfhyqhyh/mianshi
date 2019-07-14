59d2c6529a1bf535ae62f68bbaa6af713e2713c1
hsnyxfhyqhyh <xfyang46@gmail.com>
2019-07-14 10:08:06 -0400

    add new exercise static block

---
+++ javaCores/_Exercise/com/ying/exercise/core/_02ClassStaticBlock.java
@@ -0,0 +1,22 @@
+package com.ying.exercise.core;
+
+public class _02ClassStaticBlock {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+//TODO: 
+/*
+ * Create an Employee class, with 3 properties. 
+ * 	name:
+ * 	salary:
+ * 	ceo: static;  
+ * 			initialize the ceo in static block, need to understand the static is called only once and before any instantiation of objects of the class. 
+ *  
+ *  create a constructor with name/salary parameters. 
+ *  create a toString method with all information of an instance.   
+ */ 
+
